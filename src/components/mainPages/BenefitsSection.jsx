e1f6c5be40b0291064a3e556b720f2f12347cff5
Arthur Lorenzetti <tucaa222@gmail.com>
2025-01-30 13:02:58 -0300

    Responsividade perfeita até 300px

--- src/components/mainPages/BenefitsSection.jsx
+++ src/components/mainPages/BenefitsSection.jsx
@@ -1,6 +1,104 @@
+// import React from "react";
+// import "@fontsource/itim";
+// import "@fontsource/inria-sans"
+// import Cardapio from '../../assets/images/beneficios/Cardapio.png';
+// import NutriGe from '../../assets/images/beneficios/NutricionistaGerusa.png';
+// import Exercicios from '../../assets/images/beneficios/Exercicios.png';
+// import Planejamento from '../../assets/images/beneficios/Planejamento.png';
+// import WhatsApp from '../../assets/images/beneficios/WhatsApp.png';
+// import PercaPeso from '../../assets/images/beneficios/PercaPeso.png';
+
+// const BenefitsSection = () => {
+//     return (
+//       <div className="font-inria text-xl flex justify-center items-center flex-col md:p-10 p-2">
+//         <div className=" text-purple-default mb-9 ml-9 mr-9 mx-auto border-x-4 border-green-light transform skew-x-6 border-y-4 skew-y-6 rounded-lg bg-green-light md:w-96">
+//             <h1 className="font-italianno text-6xl p-3">
+//                 Por que escolher o Start 21?
+//             </h1>
+//         </div>
+
+//         <div className="xl:flex xl:flex-row md:justify-center md:items-center">
+//           <div className="flex justify-center items-center m-12 mb-9 gap-4">
+//             <div className="text-purple-default px-5 py-9 mx-auto flex flex-col items-center text-2xl border border-purple-default rounded-lg shadow-xl md:w-96 md:h-80 sm:w-80 sm:h-96">
+//               <img
+//                   src={Cardapio}
+//                   alt="Imagem de um prato com garfo e faca representando que teremos cardápios diferentes."
+//                   className="w-28 h-28 rounded-full ml-4 mr-4"
+//               />
+//               <p className="mt-5">Receba cardápios exclusivos e dicas práticas!</p>
+//               <p className="text-black text-xl mt-5">Refeições planejadas, fáceis e práticas, para simplificar a sua rotina.</p>
+//             </div>
+//           </div>
+
+//           <div className="flex justify-center items-center m-12 mb-9 gap-4">
+//             <div className="text-purple-default px-4 py-9 mx-auto flex flex-col items-center text-2xl border border-purple-default rounded-lg shadow-xl md:w-96 md:h-80 sm:w-80 sm:h-96">
+//               <img
+//                   src={NutriGe}
+//                   alt="Imagem da Nutri Gerusa representando o contato direto durante o programa."
+//                   className="w-28 h-28 rounded-full ml-4 mr-4"
+//               />
+//               <p className="mt-5">Contato direto com a Nutri Gerusa durante todo o programa!</p>
+//               <p className="text-black text-xl mt-5">Acompanhamento dedicado para te ajudar a alcançar seus objetivos.</p>
+//             </div>
+//           </div>
+//           <div className="flex justify-center items-center m-12 mb-9 gap-4">
+//             <div className="text-purple-default px-4 py-9 mx-auto flex flex-col items-center text-2xl border border-purple-default rounded-lg shadow-xl md:w-96 md:h-80 sm:w-80 sm:h-96">
+//               <img
+//                   src={Exercicios}
+//                   alt="Imagem de pessoas fazendo exercícios representando as dicas de exercícios durante o programa."
+//                   className="w-28 h-28 rounded-full ml-4 mr-4"
+//               />
+//               <p className="mt-8">Dicas de treino!</p>
+//               <p className="text-black text-xl mt-5">Exercícios que complementam sua transformação física e mental.</p>
+//             </div>
+//           </div>
+          
+//         </div>
+//         <div className="xl:flex xl:flex-row md:justify-center md:items-center">
+//           <div className="flex justify-center items-center m-12 mb-9 gap-4">
+//             <div className="text-purple-default px-4 py-9 mx-auto flex flex-col items-center text-2xl border border-purple-default rounded-lg shadow-xl md:w-96 md:h-80 sm:w-80 sm:h-96">
+//               <img
+//                   src={Planejamento}
+//                   alt="Imagem de calendário e um relógio representando a organização e o planejamento da rotina que terá durante o programa."
+//                   className="w-28 h-28 rounded-full ml-4 mr-4"
+//               />
+//               <p className="mt-5">Organização e planejamento para uma rotina equilibrada!</p>
+//               <p className="text-black text-xl mt-5">Alinhe sua alimentação, treinos e dia a dia para resultados consistentes.</p>
+//             </div>
+//           </div>
+//           <div className="flex justify-center items-center m-12 mb-9 gap-4">
+//             <div className="text-purple-default px-4 py-9 mx-auto flex flex-col items-center text-2xl border border-purple-default rounded-lg shadow-xl md:w-96 md:h-80 sm:w-80 sm:h-96">
+//               <img
+//                   src={WhatsApp}
+//                   alt="Imagem de uma pessoa segurando um celular com o ícone do WhatsApp representando o grupo exclusivo no WhatsApp."
+//                   className="w-28 h-28 rounded-full ml-4 mr-4"
+//               />
+//               <p className="mt-5">Faça parte do grupo exclusivo no WhatsApp!</p>
+//               <p className="text-black text-xl mt-5">Receba suporte diário e compartilhe sua jornada com outras pessoas.</p>
+//             </div>
+//           </div>
+
+//           <div className="flex justify-center items-center m-12 mb-9 gap-4">
+//             <div className="text-purple-default px-4 py-9 mx-auto flex flex-col items-center text-2xl border border-purple-default rounded-lg shadow-xl md:w-96 md:h-80 sm:w-80 sm:h-96">
+//               <img
+//                   src={PercaPeso}
+//                   alt="Imagem de uma pessoa com a calça larga representando a perda de peso que terá durante o programa."
+//                   className="w-28 h-28 rounded-full ml-4 mr-4"
+//               />
+//               <p className="mt-5">Resultados rápidos e sustentáveis!</p>
+//               <p className="text-black text-xl mt-5">Perca peso de forma saudável e mantenha o progresso após o programa.</p>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   };
+
+// export default BenefitsSection;
+
 import React from "react";
 import "@fontsource/itim";
-import "@fontsource/inria-sans"
+import "@fontsource/inria-sans";
 import Cardapio from '../../assets/images/beneficios/Cardapio.png';
 import NutriGe from '../../assets/images/beneficios/NutricionistaGerusa.png';
 import Exercicios from '../../assets/images/beneficios/Exercicios.png';
@@ -9,89 +107,77 @@ import WhatsApp from '../../assets/images/beneficios/WhatsApp.png';
 import PercaPeso from '../../assets/images/beneficios/PercaPeso.png';
 
 const BenefitsSection = () => {
-    return (
-      <div className="font-inria text-xl flex justify-center items-center flex-col md:p-10 p-2">
-        <div className=" text-purple-default mb-9 ml-9 mr-9 mx-auto border-x-4 border-green-light transform skew-x-6 border-y-4 skew-y-6 rounded-lg bg-green-light md:w-96">
-            <h1 className="font-italianno text-6xl p-3">
-                Por que escolher o Start 21?
-            </h1>
+  return (
+    <div className="font-inria text-xl flex justify-center items-center flex-col md:p-10 p-2">
+      <div className="text-purple-default mb-9 ml-9 mr-9 mx-auto border-x-4 border-green-light transform skew-x-6 border-y-4 skew-y-6 rounded-lg bg-green-light md:w-96">
+        <h1 className="font-italianno text-6xl p-3">
+          Por que escolher o Start 21?
+        </h1>
+      </div>
+
+      <div className="flex flex-wrap justify-center items-center gap-5">
+        <div className="flex flex-col justify-center items-center m-5 border border-purple-default rounded-lg shadow-xl md:w-96 md:h-80 sm:w-80 sm:h-96">
+          <img
+            src={Cardapio}
+            alt="Imagem de um prato com garfo e faca representando que teremos cardápios diferentes."
+            className="w-28 h-28 rounded-full mt-5"
+          />
+          <p className="mt-5 text-purple-default text-2xl">Receba cardápios exclusivos e dicas práticas!</p>
+          <p className="text-black text-xl mt-5 px-4 mb-5 sm:mb-0">Refeições planejadas, fáceis e práticas, para simplificar a sua rotina.</p>
         </div>
 
-        <div className="xl:flex xl:flex-row md:justify-center md:items-center">
-          <div className="flex justify-center items-center m-12 mb-9 gap-4">
-            <div className="text-purple-default px-5 py-9 mx-auto flex flex-col items-center text-2xl border border-purple-default rounded-lg shadow-xl md:w-96 md:h-80 sm:w-80 sm:h-96">
-              <img
-                  src={Cardapio}
-                  alt="Imagem de um prato com garfo e faca representando que teremos cardápios diferentes."
-                  className="w-28 h-28 rounded-full ml-4 mr-4"
-              />
-              <p className="mt-5">Receba cardápios exclusivos e dicas práticas!</p>
-              <p className="text-black text-xl mt-5">Refeições planejadas, fáceis e práticas, para simplificar a sua rotina.</p>
-            </div>
-          </div>
-
-          <div className="flex justify-center items-center m-12 mb-9 gap-4">
-            <div className="text-purple-default px-4 py-9 mx-auto flex flex-col items-center text-2xl border border-purple-default rounded-lg shadow-xl md:w-96 md:h-80 sm:w-80 sm:h-96">
-              <img
-                  src={NutriGe}
-                  alt="Imagem da Nutri Gerusa representando o contato direto durante o programa."
-                  className="w-28 h-28 rounded-full ml-4 mr-4"
-              />
-              <p className="mt-5">Contato direto com a Nutri Gerusa durante todo o programa!</p>
-              <p className="text-black text-xl mt-5">Acompanhamento dedicado para te ajudar a alcançar seus objetivos.</p>
-            </div>
-          </div>
-          <div className="flex justify-center items-center m-12 mb-9 gap-4">
-            <div className="text-purple-default px-4 py-9 mx-auto flex flex-col items-center text-2xl border border-purple-default rounded-lg shadow-xl md:w-96 md:h-80 sm:w-80 sm:h-96">
-              <img
-                  src={Exercicios}
-                  alt="Imagem de pessoas fazendo exercícios representando as dicas de exercícios durante o programa."
-                  className="w-28 h-28 rounded-full ml-4 mr-4"
-              />
-              <p className="mt-8">Dicas de treino!</p>
-              <p className="text-black text-xl mt-5">Exercícios que complementam sua transformação física e mental.</p>
-            </div>
-          </div>
-          
+        <div className="flex flex-col justify-center items-center m-5 border border-purple-default rounded-lg shadow-xl md:w-96 md:h-80 sm:w-80 sm:h-96">
+          <img
+            src={NutriGe}
+            alt="Imagem da Nutri Gerusa representando o contato direto durante o programa."
+            className="w-28 h-28 rounded-full mt-5"
+          />
+          <p className="mt-5 text-purple-default text-2xl">Contato direto com a Nutri Gerusa durante todo o programa!</p>
+          <p className="text-black text-xl mt-5 px-4 mb-5 sm:mb-0">Acompanhamento dedicado para te ajudar a alcançar seus objetivos.</p>
         </div>
-        <div className="xl:flex xl:flex-row md:justify-center md:items-center">
-          <div className="flex justify-center items-center m-12 mb-9 gap-4">
-            <div className="text-purple-default px-4 py-9 mx-auto flex flex-col items-center text-2xl border border-purple-default rounded-lg shadow-xl md:w-96 md:h-80 sm:w-80 sm:h-96">
-              <img
-                  src={Planejamento}
-                  alt="Imagem de calendário e um relógio representando a organização e o planejamento da rotina que terá durante o programa."
-                  className="w-28 h-28 rounded-full ml-4 mr-4"
-              />
-              <p className="mt-5">Organização e planejamento para uma rotina equilibrada!</p>
-              <p className="text-black text-xl mt-5">Alinhe sua alimentação, treinos e dia a dia para resultados consistentes.</p>
-            </div>
-          </div>
-          <div className="flex justify-center items-center m-12 mb-9 gap-4">
-            <div className="text-purple-default px-4 py-9 mx-auto flex flex-col items-center text-2xl border border-purple-default rounded-lg shadow-xl md:w-96 md:h-80 sm:w-80 sm:h-96">
-              <img
-                  src={WhatsApp}
-                  alt="Imagem de uma pessoa segurando um celular com o ícone do WhatsApp representando o grupo exclusivo no WhatsApp."
-                  className="w-28 h-28 rounded-full ml-4 mr-4"
-              />
-              <p className="mt-5">Faça parte do grupo exclusivo no WhatsApp!</p>
-              <p className="text-black text-xl mt-5">Receba suporte diário e compartilhe sua jornada com outras pessoas.</p>
-            </div>
-          </div>
 
-          <div className="flex justify-center items-center m-12 mb-9 gap-4">
-            <div className="text-purple-default px-4 py-9 mx-auto flex flex-col items-center text-2xl border border-purple-default rounded-lg shadow-xl md:w-96 md:h-80 sm:w-80 sm:h-96">
-              <img
-                  src={PercaPeso}
-                  alt="Imagem de uma pessoa com a calça larga representando a perda de peso que terá durante o programa."
-                  className="w-28 h-28 rounded-full ml-4 mr-4"
-              />
-              <p className="mt-5">Resultados rápidos e sustentáveis!</p>
-              <p className="text-black text-xl mt-5">Perca peso de forma saudável e mantenha o progresso após o programa.</p>
-            </div>
-          </div>
+        <div className="flex flex-col justify-center items-center m-5 border border-purple-default rounded-lg shadow-xl md:w-96 md:h-80 sm:w-80 sm:h-96">
+          <img
+            src={Exercicios}
+            alt="Imagem de pessoas fazendo exercícios representando as dicas de exercícios durante o programa."
+            className="w-28 h-28 rounded-full mt-5"
+          />
+          <p className="mt-5 text-purple-default text-2xl">Dicas de treino!</p>
+          <p className="text-black text-xl mt-5 px-4 mb-5 sm:mb-0">Exercícios que complementam sua transformação física e mental.</p>
+        </div>
+
+        <div className="flex flex-col justify-center items-center m-5 border border-purple-default rounded-lg shadow-xl md:w-96 md:h-80 sm:w-80 sm:h-96">
+          <img
+            src={Planejamento}
+            alt="Imagem de calendário e um relógio representando a organização e o planejamento da rotina que terá durante o programa."
+            className="w-28 h-28 rounded-full mt-5"
+          />
+          <p className="mt-5 text-purple-default text-2xl">Organização e planejamento para uma rotina equilibrada!</p>
+          <p className="text-black text-xl mt-5 px-4 mb-5 sm:mb-0">Alinhe sua alimentação, treinos e dia a dia para resultados consistentes.</p>
+        </div>
+
+        <div className="flex flex-col justify-center items-center m-5 border border-purple-default rounded-lg shadow-xl md:w-96 md:h-80 sm:w-80 sm:h-96">
+          <img
+            src={WhatsApp}
+            alt="Imagem de uma pessoa segurando um celular com o ícone do WhatsApp representando o grupo exclusivo no WhatsApp."
+            className="w-28 h-28 rounded-full mt-5"
+          />
+          <p className="mt-5 text-purple-default text-2xl">Faça parte do grupo exclusivo no WhatsApp!</p>
+          <p className="text-black text-xl mt-5 px-4 mb-5 sm:mb-0">Receba suporte diário e compartilhe sua jornada com outras pessoas.</p>
+        </div>
+
+        <div className="flex flex-col justify-center items-center m-5 border border-purple-default rounded-lg shadow-xl md:w-96 md:h-80 sm:w-80 sm:h-96 ">
+          <img
+            src={PercaPeso}
+            alt="Imagem de uma pessoa com a calça larga representando a perda de peso que terá durante o programa."
+            className="w-28 h-28 rounded-full mt-5"
+          />
+          <p className="mt-5 text-purple-default text-2xl">Resultados rápidos e sustentáveis!</p>
+          <p className="text-black text-xl mt-5 px-4 mb-5 sm:mb-0">Perca peso de forma saudável e mantenha o progresso após o programa.</p>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default BenefitsSection;
--- src/components/mainPages/WeekSection.jsx
+++ src/components/mainPages/WeekSection.jsx
@@ -1,3 +1,101 @@
+// import React from "react";
+// import "@fontsource/italianno";
+// import "@fontsource/inria-sans"
+// import FocoTotal from '../../assets/images/FocoTotal.png';
+// import SemPerderORitmo from '../../assets/images/SemPerderORitmo.png';
+// import RumoAConsistencia from '../../assets/images/RumoAConsistencia.png';
+
+// const WeekSection = () => {
+//   return (
+//     <div className="font-inria text-xl flex flex-col items-center justify-center md:gap-5 md:p-10 p-2">         
+//       <div>
+//         <div className="bg-yellow-default text-purple-dark bg-opacity-35 rounded-full sm:w-[360px] sm:h-[360px] w-[290px] h-[290px] sm:p-10 p-3 mx-auto">
+//           <p className="mt-6">Prepare-se para</p>
+//           <p className="sm:mb-4 mb-2">o Carnaval com um programa único e estruturado que combina nutrição, exercícios e organização.</p>
+//           <p className="sm:mt-5 sm:mb-4 mt-1">Você tera o suporte necessário para alcançar resultados sustentáveis!</p>
+//         </div>
+//       </div>
+
+//       <div className="flex xl:flex-row xl:justify-center xl:items-center flex-col justify-center items-center gap-1">
+//         <div className="lg:flex lg:flex-row lg:justify-center lg:items-start xl:items-center gap-1">
+//           <div className="flex flex-col justify-center items-center md:m-2 m-5 mb-16 border-2 border-purple-dark rounded-3xl bg-purple-light shadow-right-lg p-5 gap-5 md:mb-0 lg:py-8 xl:py-5">
+//             <div className="text-purple-default mx-auto">
+//               <p className="font-italianno text-6xl">Semana 1</p>
+//               <p className="underline font-bold">Foco Total!</p>
+//             </div>
+//             <hr className="border-t-2 border-gray-500 w-3/4 mx-auto mb-6"/>
+//             <img
+//                   src={FocoTotal}
+//                   alt="Imagem Semana 1 - queimando a sua gordura."
+//                   className="w-28 h-28 rounded-full ml-4 mr-4"
+//               />
+//             <p className="md:w-96">
+//               Nessa primeira semana, você dará o pontapé inicial na transformação do seu corpo e saúde.
+//             </p>
+//             <p className="md:w-96">
+//               O objetivo é acelerar o metabolismo, ativar a queima de gordura e preparar seu organismo para as próximas etapas.
+//             </p>
+//             <p className="md:w-96">
+//               Com cardápios estratégicos, dicas práticas e foco total, você começará a sentir os primeiros resultados enquanto ganha mais energia e disposição!
+//             </p>
+//           </div>
+
+//           <div className="flex flex-col justify-center items-center md:m-2 m-5 mb-9 border-2 border-purple-dark rounded-3xl bg-purple-light shadow-right-lg p-5 gap-5 ">
+//             <div className="text-purple-default mx-auto">
+//               <p className="font-italianno text-6xl">Semana 2</p>
+//               <p className="underline font-bold">Sem Perder o Ritmo!</p>
+//             </div>
+//             <hr className="border-t-2 border-gray-500 w-3/4 mx-auto mb-6"/>
+//             <img
+//                   src={SemPerderORitmo}
+//                   alt="Imagem Semana 2 - planejando sua rotina."
+//                   className="w-28 h-28 rounded-full ml-4 mr-4"
+//               />
+//             <p className="md:w-96">
+//               Essa semana é dedicada a manter o foco enquanto você aperfeiçoa sua rotina.
+//             </p>
+//             <p className="md:w-96">
+//               Com organização e planejamento, você aprenderá a ter mais controle sobre sua alimentação e hábitos diários.
+//             </p>
+//             <p className="md:w-96">
+//               Além disso, as dicas de treino vão ajudar a intensificar os resultados, garantindo disposição e motivação para continuar firme no programa!
+//             </p>
+//             <p  className="md:w-96">
+//               💪💪💪
+//             </p>
+            
+//           </div>
+//         </div>
+
+//         <div className="flex flex-col justify-center items-center md:m-2 m-5 mb-9 md:mt-1 border-2 border-purple-dark rounded-3xl bg-purple-light shadow-right-lg p-5 gap-5">
+//             <div className="text-purple-default mx-auto">
+//                 <p className="font-italianno text-6xl">Semana 3</p>
+//                 <p className="underline font-bold">Rumo à Consistência!</p>
+//             </div>
+//             <hr className="border-t-2 border-gray-500 w-3/4 mx-auto mb-6"/>
+//             <img
+//                 src={RumoAConsistencia}
+//                 alt="Imagem Semana 3 - rumo à consistência."
+//                 className="w-28 h-28 rounded-full ml-4 mr-4"
+//             />
+//             <p className="md:w-96">
+//                 Na última semana, o foco está em criar hábitos que você pode levar para a vida toda de maneira duradoura.
+//             </p>
+//             <p className="md:w-96">
+//                 Com um cardápio prático e equilibrado, sem muitas restrições, você aprenderá a manter uma rotina leve e sustentável.
+//             </p>
+//             <p className="md:w-96">
+//                 Essa é a etapa em que consolidamos tudo o que foi trabalhado, garantindo que você continue colhendo os benefícios muito além dos 21 dias!
+//             </p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default WeekSection;
+
+
 import React from "react";
 import "@fontsource/italianno";
 import "@fontsource/inria-sans"
@@ -15,10 +113,10 @@ const WeekSection = () => {
           <p className="sm:mt-5 sm:mb-4 mt-1">Você tera o suporte necessário para alcançar resultados sustentáveis!</p>
         </div>
       </div>
-      <div className="flex xl:flex-row xl:justify-center xl:items-center flex-col justify-center items-center gap-1">
 
+      <div className="flex xl:flex-row xl:justify-center xl:items-center md:flex-col flex-wrap justify-center items-center gap-1">
         <div className="lg:flex lg:flex-row lg:justify-center lg:items-start xl:items-center gap-1">
-          <div className="flex flex-col justify-center items-center md:m-2 m-5 mb-16 border-2 border-purple-dark rounded-3xl bg-purple-light shadow-right-lg p-5 gap-5 md:mb-0 lg:py-8 xl:py-5">
+          <div className="flex flex-col justify-center items-center md:m-2 m-5 mb-16 border-2 border-purple-dark rounded-3xl bg-purple-light shadow-right-lg p-5 gap-5">
             <div className="text-purple-default mx-auto">
               <p className="font-italianno text-6xl">Semana 1</p>
               <p className="underline font-bold">Foco Total!</p>
@@ -52,17 +150,18 @@ const WeekSection = () => {
                   className="w-28 h-28 rounded-full ml-4 mr-4"
               />
             <p className="md:w-96">
-              Essa semana é dedicada a manter o foco enquanto você aperfeiçoa sua rotina.
+              Essa semana é dedicada a manter o foco enquanto você aperfeiçoa sua rotina. Com um cardápio especifico e exercícios. 
             </p>
             <p className="md:w-96">
               Com organização e planejamento, você aprenderá a ter mais controle sobre sua alimentação e hábitos diários.
+
             </p>
             <p className="md:w-96">
               Além disso, as dicas de treino vão ajudar a intensificar os resultados, garantindo disposição e motivação para continuar firme no programa!
             </p>
-            <p  className="md:w-96">
+            {/* <p  className="md:w-96">
               💪💪💪
-            </p>
+            </p> */}
             
           </div>
         </div>
